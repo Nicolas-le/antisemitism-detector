45425586cf945b9897ea18aecf236248b82592a0
Nicolas-le <nicolas.ruth@web.de>
2021-01-24 15:30:18 +0100

    structure

--- data_set/4chan_scraper.py
+++ data_set/4chan_scraper.py
@@ -9,18 +9,26 @@ def get_IDs():
 
     # iterating over thread_IDs_file and call get_thread_json() every second
 
-
 def get_thread_Json(thread_ID):
     thread_json = requests.get("https://a.4cdn.org/pol/thread/"+str(thread_ID)+".json")
     preprocess_json(thread_json)
 
 def preprocess_json(thread_json):
     # take whole json per thread and delete unuseful information
-
-
     store_to_db(preprocessed_json)
 
 def store_to_db(preprocessed_json):
     # save json to tinydb
 
 
+"""
+iterate over every tinydb entry:
+
+for thread in db.__iter__():
+    do something with thread, f.ex.:
+    db.update({'keyword': ["jew"]}, doc_ids=[thread.doc_id])
+    https://tinydb.readthedocs.io/en/stable/usage.html
+    works even if keyword isn't originally a key in the thread
+
+    db.close() wichtig für schreiben von Änderungen
+"""
