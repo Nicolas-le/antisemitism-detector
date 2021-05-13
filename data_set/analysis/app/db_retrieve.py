1080162214b919106fbcc46a1126cb36b3b6653f
Nicolas-le <nicolas.ruth@web.de>
2021-05-13 20:05:42 +0200

    get post per day

--- data_set/analysis/app/db_retrieve.py
+++ data_set/analysis/app/db_retrieve.py
@@ -1,15 +1,18 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB, Query, where
 
 
 class DBRetrieval:
     def __init__(self):
         self.database = TinyDB('../../4chan_pol_database.json')
+        self.test_contains = lambda value, search: search in value
+
 
     def get_post_per_day(self,date):
-        # "posting_time": "05/12/21(Wed)06:12:07"
+        # "posting_time": "05/12/21"
         query = Query()
-        return self.database.search(query["posting_time"] == date)
+        return self.database.search(query.posting_time.test(self.test_contains, date))
+
 
 retrieval = DBRetrieval()
-
-posts_of_date = retrieval.get_post_per_day("05/12/21(Wed)06:12:07")
+date = "05/12/21"
+posts_of_date = retrieval.get_post_per_day(date)
